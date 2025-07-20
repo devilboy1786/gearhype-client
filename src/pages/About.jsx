ceca94727662edc8df949364952a25a8f6242807
devilboy1786 <devilsdad356@gmail.com>
2025-07-20 09:35:25 +0500

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,3 @@
-import React from 'react';
-
-const About = () => {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">About Us</h1>
-      <p>This is the about page for GearHype.</p>
-    </div>
-  );
-};
-
-export default About;
+export default function About() {
+  return <h1 className="text-2xl font-bold">About Us</h1>;
+}
