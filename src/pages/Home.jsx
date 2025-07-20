3aea2a504766a888db1c74e34b4be69fdaabe494
devilboy1786 <devilsdad356@gmail.com>
2025-07-20 09:34:24 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,3 @@
-import React from 'react';
-
-const Home = () => {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Welcome to GearHype</h1>
-      <p>This is the homepage.</p>
-    </div>
-  );
-};
-
-export default Home;
+export default function Home() {
+  return <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>;
+}
